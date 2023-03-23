b6d89cadc5f36bfe0a0b404760ef5ebd5f3a150c
Felix Warner (RIT Student) <fgw7468@rit.edu>
2023-03-23 12:17:03 -0400

    Tree reconstruction

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { Tile } from './tiles/api.js';
 
-import { fireGirl, uh } from './tiles/imageTiles/index.js';
+import { fireGirl, uh, mountainsTrees, bark, clouds, sky, branches, sun } from './tiles/imageTiles/index.js';
 import { rasterize } from './tiles/observe/rasterize.js';
 import { renderRaster } from './tiles/observe/renderRaster.js';
 
 const width = 600,
-      height = 600;
+  height = 600;
 
 const canvas = document.querySelector('#canvas');
 canvas.width = width;
@@ -13,22 +13,115 @@ canvas.height = height;
 
 const ctx = canvas?.getContext('2d');
 
-const tile = Tile.swirl(
+const tile = Tile.above(
   Tile.above(
-    Tile.swirl(
-      Tile.above(
-        Tile.pure('red'),
-        fireGirl
-      )
+    Tile.beside(
+      Tile.counterclockwise(branches),
+      sky,
     ),
     Tile.beside(
-      uh, 
-      Tile.pure('blue')
+      sky,
+      sky,
+      Tile.above(
+        sky,
+        sky,
+        Tile.beside(sky, bark),
+      ),
+      bark,
+      sun,
+      Tile.clockwise(branches),
+      sky,
+      sky,
+      sky,
+      sky,
+      Tile.above(
+        sky,
+        bark,
+      ),
+    ),
+  ),
+
+  Tile.beside(
+    Tile.quad(
+      clouds,
+      Tile.quad(
+        Tile.quad(
+          bark,
+          Tile.beside(
+            bark,
+            sky
+          ),
+          bark,
+          bark
+        ),
+        sky,
+        bark,
+        Tile.quad(
+          bark,
+          sky,
+          bark,
+          bark
+        )
+      ),
+      mountainsTrees,
+      bark
+    ),
+    Tile.quad(
+      Tile.quad(
+        sky,
+        Tile.quad(
+          sky,
+          sky,
+          sky,
+          bark
+        ),
+        Tile.quad(
+          sky,
+          sky,
+          sky,
+          bark
+        ),
+        bark
+      ),
+      Tile.beside(
+        sky,
+        bark
+      ),
+      bark,
+      Tile.beside(
+        Tile.beside(
+          Tile.above(
+            Tile.above(
+              sky,
+              mountainsTrees
+            ),
+            bark
+          ),
+          bark
+        ),
+        bark
+      )
     )
   )
-);
+)
+  ;
 
-const raster = rasterize({width, height})(tile);
+// const tile = Tile.swirl(
+//   Tile.above(
+//     Tile.swirl(
+//       Tile.above(
+//         Tile.pure('red'),
+//         fireGirl
+//       )
+//     ),
+//     Tile.beside(
+//       uh, 
+//       Tile.pure('blue')
+//     )
+//   )
+// );
+
+const raster = rasterize({ width, height })(tile);
 
 renderRaster(raster)(ctx);
 
